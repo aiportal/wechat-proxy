bb63ce92e0f16c6613945c9f11a872a0de2d0e16
aiportal <glave666@gmail.com>
2017-07-25 10:35:42 +0800

    Store interface{}.

--- wxproxy/cache.go
+++ wxproxy/cache.go
@@ -6,34 +6,35 @@ import (
 )
 
 type cacheItem struct {
-	value  string
+	value  interface{}
 	expire int64
 }
 
 type cacheMap struct {
 	m    map[string]cacheItem
 	lock sync.RWMutex
-
-	duration time.Duration // duration for cache item in memory.
-	limit    int           // limit for cache item count.
+	config struct {
+		duration time.Duration // duration for cache item in memory.
+		limit    int           // limit for cache item count.
+	}
 }
 
 func NewCacheMap(duration time.Duration, limit int) *cacheMap {
 	tm := new(cacheMap)
 	tm.m = make(map[string]cacheItem)
-	tm.duration = duration
-	tm.limit = limit
+	tm.config.duration = duration
+	tm.config.limit = limit
 	return tm
 }
 
-func (tm *cacheMap) Set(key string, value string) {
-	expire := time.Now().Add(tm.duration).Unix()
+func (tm *cacheMap) Set(key string, value interface{}) {
+	expire := time.Now().Add(tm.config.duration).Unix()
 	tm.lock.Lock()
 	defer tm.lock.Unlock()
 	tm.m[key] = cacheItem{value: value, expire: expire}
 }
 
-func (tm *cacheMap) Get(key string) (value string, success bool) {
+func (tm *cacheMap) Get(key string) (value interface{}, success bool) {
 	tm.lock.RLock()
 	defer tm.lock.RUnlock()
 	if token, ok := tm.m[key]; ok {
@@ -67,7 +68,7 @@ func (tm *cacheMap) Clean() {
 }
 
 func (tm *cacheMap) Shrink() {
-	if tm.Len() > tm.limit {
+	if tm.Len() > tm.config.limit {
 		go tm.Clean()
 	}
 }
