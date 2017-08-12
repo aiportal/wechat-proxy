d4d73bcbe470d520a3b35bcc8e80ea83e05dff2e
aiportal <glave666@gmail.com>
2017-08-12 18:18:09 +0800

    Start by args auth.

--- wxproxy/auth_test.go
+++ wxproxy/auth_test.go
@@ -7,9 +7,14 @@ import (
 	"fmt"
 	"log"
 	"strings"
+	"os"
 )
 
 func TestAuthServer(t *testing.T) {
+	if !argsContainsAuth() {
+		return
+	}
+
 	log.SetOutput(ioutil.Discard)
 
 	hostUrl := "http://debug.ultragis.com"
@@ -37,7 +42,6 @@ func TestAuthServer(t *testing.T) {
 			fmt.Println("body: " + string(body))
 			t.Fatal(err)
 		}
-		//fmt.Println(info.AuthUrl)
 		auth_urls[i] = info.AuthUrl
 	}
 
@@ -48,10 +52,23 @@ func TestAuthServer(t *testing.T) {
 		fmt.Println(err.String())
 		t.Fatal(err)
 	}
-	sendTestAuthLink(access_token, openid, auth_urls[0], auth_urls[1])
+
+	err1 := sendTestAuthLink(access_token, openid, auth_urls[0], auth_urls[1])
+	if err1 != nil {
+		t.Fatal(err1)
+	}
 }
 
-func sendTestAuthLink(accessToken, openid, link_base, link_info string) {
+func argsContainsAuth() bool {
+	for _, a := range os.Args {
+		if a == "auth" {
+			return true
+		}
+	}
+	return false
+}
+
+func sendTestAuthLink(accessToken, openid, link_base, link_info string) (err error) {
 	sendUrl := "https://api.weixin.qq.com/cgi-bin/message/custom/send"
 	send_url := fmt.Sprintf("%s?access_token=%s", sendUrl, accessToken)
 
@@ -80,69 +97,14 @@ func sendTestAuthLink(accessToken, openid, link_base, link_info string) {
 
 	resp, err := http.Post(send_url, "", strings.NewReader(data))
 	if err != nil {
-		fmt.Println(err.Error())
 		return
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err.Error())
 		return
 	}
 	fmt.Println(string(body))
-}
 
-//
-//func TestAuthInit(t *testing.T) {
-//
-//	ts_init := []struct{
-//		Url string
-//		Redirect string
-//		Fields []string
-//	}{
-//		{
-//			Url: "/auth?appid=wx000&secret=xxxxxx",
-//			Redirect: "/echo",
-//			Fields:[]string{"auth_uri", "expires_in"},
-//		},
-//		{
-//			Url: "/auth?appid=wx000",
-//			Redirect: "/echo",
-//			Fields:[]string{"errcode", "errmsg"},
-//		},
-//	}
-//
-//	ts := httptest.NewServer(NewAuthServer())
-//	defer ts.Close()
-//
-//	for _, v := range ts_init {
-//		_url := fmt.Sprintf("%s%s&redirect_uri=%s", ts.URL, v.Url, url.PathEscape(ts.URL+v.Redirect))
-//		resp, err := http.Get(_url)
-//		if err != nil {
-//			log.Fatal(err)
-//		}
-//		fmt.Println(_url)
-//
-//		body, err := ioutil.ReadAll(resp.Body)
-//		resp.Body.Close()
-//		if err != nil {
-//			log.Fatal(err)
-//		}
-//		//fmt.Printf("body: %s\n", string(body))
-//
-//		var f interface{}
-//		err = json.Unmarshal(body, &f)
-//		if err != nil {
-//			fmt.Printf("url: %s\n", _url)
-//			fmt.Printf("body: %s\n", string(body))
-//			log.Fatal(err)
-//		}
-//
-//		m := f.(map[string]interface{})
-//		for _, fld := range v.Fields {
-//			if m[fld] == nil {
-//				log.Fatal()
-//			}
-//		}
-//	}
-//}
+	return
+}
